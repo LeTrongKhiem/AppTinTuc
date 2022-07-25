674ecaddf1a481d792e0e01fc65a232aa4e50b82
19130004 <19130004@st.hcmuaf.edu.vn>
2022-07-25 22:00:02 +0700

    upload service

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import {NewsApiService} from "./Service/news-api.service";
+import {HttpClientModule} from "@angular/common/http";
+import { FooterComponent } from './footer/footer.component';
+import { DomesticComponent } from './domestic/domestic.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    DomesticComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
