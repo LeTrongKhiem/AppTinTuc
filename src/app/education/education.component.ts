7f808571c17bbaef862efb36d888431037a48fef
LeTrongKhiem <19130102@st.hcmuaf.edu.vn>
2022-07-27 22:54:05 +0700

    call api page healthy and education and fix ui

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NewsApiService} from "../Service/news-api.service";
 
 @Component({
   selector: 'app-education',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education.component.css', '../commoncomponent/commoncomponent.component.css']
 })
 export class EducationComponent implements OnInit {
+  giaoducResult : any = [];
+  totalLenght : any;
+  page : number = 1;
 
-  constructor() { }
+  constructor(private service : NewsApiService) {
+    this.getItems();
+  }
 
   ngOnInit(): void {
+  }
+  getItems():void{
+    this.service.giaoDucApi().subscribe((result) => {
+      this.giaoducResult = result.items;
+      this.totalLenght = result.item.length;
+      console.log(result);
+    })
   }
 
 }
