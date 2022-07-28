5b666912c4e7d06f6127f0d2ad4aadc5b34aeb4f
tangm <19130223@st.hcmuaf.edu.vn>
2022-07-28 18:36:41 +0700

    add scroll top

---
+++ src/app/scroll-to-top/scroll-to-top.component.ts
@@ -0,0 +1,34 @@
+import { Component,HostListener, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scroll-to-top',
+  templateUrl: './scroll-to-top.component.html',
+  styleUrls: ['./scroll-to-top.component.css']
+})
+export class ScrollToTopComponent implements OnInit {
+  showScroll: boolean =false;
+  showScrollHeight = 300;
+  hideScrollHeight = 10;
+  constructor() { }
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    if((window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop) > this.showScrollHeight) {
+      this.showScroll = true;
+    }
+    else if (this.showScroll && (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop) < this.hideScrollHeight) {
+      this.showScroll = false;
+    }
+
+  }
+  ngOnInit(): void {
+  }
+  scrollToTop() {
+    (function smoothscroll() {
+      var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0) {
+        window.requestAnimationFrame(smoothscroll);
+        window.scrollTo(0, currentScroll - (currentScroll / 5));
+      }
+    })();
+  }
+}
