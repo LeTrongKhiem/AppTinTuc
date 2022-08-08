335031bc4eb3c05c663ecc1fd72b581d8a49ca33
19130004 <19130004@st.hcmuaf.edu.vn>
2022-08-08 22:25:56 +0700

    add catelory youread

---
+++ src/app/you-read/you-read.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {NewsApiService} from "../Service/news-api.service";
+
+@Component({
+  selector: 'app-you-read',
+  templateUrl: './you-read.component.html',
+  styleUrls: ['./you-read.component.css','../domestic/domestic.component.css','../app.component.css']
+})
+export class YouReadComponent implements OnInit {
+
+  banDocResult: any = [];
+  totalLength:any;
+  page:number=1;
+  constructor(private service: NewsApiService) { }
+  ngOnInit(): void {
+    this.service.banDocApi().subscribe((result) => {
+      // console.log(result);
+      this.banDocResult = result.items;
+      this.totalLength = result.items.length;
+      console.log(result)
+    })
+  }
+
+}
